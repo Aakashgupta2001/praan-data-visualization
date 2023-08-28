d87bc36cfdfa7b8c35f3d984e99390e43f5ac750
Aakash Gupta <aakashgupta2001.2909@gmail.com>
2023-08-28 21:06:26 +0530

    backend init

---
+++ index.js
@@ -0,0 +1,13 @@
+const express = require("express");
+const app = express();
+const cors = require("cors");
+
+app.use(cors());
+
+//starting server
+const port = process.env.PORT || 3001;
+app.listen(port, (err) => {
+  if (err) {
+    console.log(err);
+  } else console.log("Server is running on port", port);
+});
